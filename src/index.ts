3da98973c9f140b40037c8ffb7fecc28153689c8
smalandrey67 <smalandrey67@ukr.net>
2023-02-26 14:17:24 +0200

    delete ui CategoryElement, update version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 import { Button } from "./components/Button/Button";
-import { CategoryElement } from "./components/CategoryElement/CategoryElement";
-
-export { Button, CategoryElement };
+export { Button };
